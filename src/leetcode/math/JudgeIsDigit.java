4f112ebe6e6d147b708fdba3cb6ee9fd46e8ca32
liuxinglin <1103931539@qq.com>
2020-06-18 22:50:04 +0800

    Add files via upload

---
+++ src/leetcode/math/JudgeIsDigit.java
@@ -0,0 +1,23 @@
+package math;
+
+/**
+ * 判断double类型数是否是整数
+ *
+ * 方法1：对1取模，结果为0表示是整数
+ */
+
+public class JudgeIsDigit {
+    public static void main(String[] args) {
+        int num = 9;
+        double sqrt = Math.sqrt(num);
+        boolean res = judge1(sqrt);
+        System.out.println(res);
+    }
+
+    public static boolean judge1(double num) {
+        if (num % 1 == 0) {
+            return true;
+        }
+        return false;
+    }
+}
